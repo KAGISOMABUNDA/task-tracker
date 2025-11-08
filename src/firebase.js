7be1d1595997f22ae874e26162663749d41f506c
Kagiso Wandile Mabunda <wxvambient.wav@gmail.com>
2025-11-08 11:18:46 +0200

    Remove Firebase configuration from firebase.js
    Removed sensitive Firebase configuration details.

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCe9HkLaTW37zPZcFNOs4vxuCLsE-hX3t4",
-  authDomain: "task-tracker-2501.firebaseapp.com",
-  projectId: "task-tracker-2501",
-  storageBucket: "task-tracker-2501.firebasestorage.app",
-  messagingSenderId: "342196802635",
-  appId: "1:342196802635:web:a446f419a315083196a9ce",
-  measurementId: "G-VXBM407TW6"
+
 };
 
 
